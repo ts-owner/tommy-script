fb868339633f1fc8ee4ed917783a82372287135f
brendan <shamrockfrost@gmail.com>
2017-11-14 12:12:46 -0800

    Changed Demo.kt to use the transpiler

--- src/compiler/python.kt
+++ src/compiler/python.kt
@@ -13,7 +13,7 @@ fun InOp.toPython() = when(this) {
     InOp.plus -> "+"
     InOp.subtract -> "-"
     InOp.times -> "*"
-    InOp.div -> "/"
+    InOp.div -> "//"
     InOp.mod -> "%"
     InOp.power -> "**"
     InOp.concat -> "+"
@@ -25,6 +25,12 @@ fun InOp.toPython() = when(this) {
     InOp.leq -> "<="
     InOp.geq -> ">="
     InOp.neq -> "!="
+}
+
+fun handleSpecialFunctions(func : FunCall) = when(func.id) {
+    "print" -> "print(${func.args.joinToString(", ", transform = ::compile)}, end='', flush=True)"
+    "println" -> "print(${func.args.joinToString(", ", transform = ::compile)})"
+    else -> null
 }
 
 fun String.escaped() : String {
@@ -43,7 +49,8 @@ private fun compile(expr : Expr) : String = when(expr) {
     is Var -> expr.id // TODO: Name scrambling?
     is Prefix -> "(${expr.op.toPython()} ${compile(expr.arg)})"
     is Infix -> "(${compile(expr.lhs)} ${expr.op.toPython()} ${compile(expr.rhs)})"
-    is FunCall -> "${expr.id}(${expr.args.joinToString("", transform = ::compile)})"
+    is FunCall -> handleSpecialFunctions(expr) ?:
+                "${expr.id}(${expr.args.joinToString(", ", transform = ::compile)})"
     is LInt -> "${expr.value}"
     is LString -> "\"${expr.value.escaped()}\""
     is LBool -> if(expr.value) "True" else "False"
@@ -55,19 +62,25 @@ private fun compile(expr : Expr) : String = when(expr) {
 private val indentStr = "    "
 
 private fun compile(ast : AST, indent : String) : String = when(ast) {
-    is Expr -> compile(ast)
+    is Expr -> indent + compile(ast)
     is Statement -> {
         val stmt : Statement = ast
         val newIndent = "$indent$indentStr"
         val end = "\n${newIndent}pass"
         fun List<AST>.joinBody() = joinToString("\n") { compile(it, newIndent) }
         when(stmt) {
-            is If -> {
-                val (cond, thenBranch, elseBranch) = stmt
-                val thenStr = thenBranch.joinBody()
-                val elseStr = elseBranch?.joinBody().orEmpty()
-                "${indent}if ${compile(cond)}:\n$thenStr$end\nelse:$elseStr$end"
+            is IfStep -> {
+                val (cond, body, next) = stmt
+                val thenStr = body.joinBody()
+                val elseStr = when(next) {
+                    is IfStep -> compile(next, indent).replaceFirst("if", "elif")
+                    is Else -> compile(next, indent)
+                    null -> ""
+                }
+                "${indent}if ${compile(cond)}:\n$thenStr$end\n$elseStr"
             }
+            // TODO: Make sure we can't transpile a standalone else
+            is Else -> "${indent}else:\n${stmt.body.joinBody()}$end"
             is VarDef -> "$indent${stmt.lhs.id} = ${compile(stmt.rhs)}"
             is UntypedVarDef -> "$indent${stmt.lhs.id} = ${compile(stmt.rhs)}"
             is VarReassign -> "$indent${stmt.lhs.id} = ${compile(stmt.rhs)}"
@@ -87,7 +100,7 @@ private fun compile(ast : AST, indent : String) : String = when(ast) {
                 val bodyStr = body.joinBody()
                 "${indent}for $id in ${compile(list)}:\n$bodyStr$end"
             }
-            is Return -> "$indent${compile(stmt.toReturn)}"
+            is Return -> "${indent}return ${compile(stmt.toReturn)}"
         }
     }
 }
@@ -95,8 +108,8 @@ private fun compile(ast : AST, indent : String) : String = when(ast) {
 fun compile(ast : AST) = compile(ast, "")
 
 fun main(args: Array<String>) {
-    val example : Statement = If(cond = Prefix(PreOp.not, Infix(InOp.eqInt, LInt(6), LInt(9))),
-                                thenBranch = listOf(UntypedVarDef(Var("x"), LInt(42)),
+    val example : Statement = IfStep(cond = Prefix(PreOp.not, Infix(InOp.eqInt, LInt(6), LInt(9))),
+                                body = listOf(UntypedVarDef(Var("x"), LInt(42)),
                                                     VarReassign(Var("x"),
                                                                 Infix(InOp.times, Var("x"), LInt(10))),
                                                     Return(LString("a\nb"))))
